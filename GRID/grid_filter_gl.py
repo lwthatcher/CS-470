28192251eec6b2c7e3d3c8d4224d2e47c4ffafcd
Lawrence William Thatcher <lthatch1@club.cs.byu.edu>
2014-10-23 10:51:58 -0600

    not working grid, but there

--- GRID/grid_filter_gl.py
+++ GRID/grid_filter_gl.py
@@ -7,37 +7,42 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from numpy import zeros
 
-grid = None
+class Grid:
+	
+	grid = None
+	
+	def __init__(self):
+		
 
-def draw_grid():
-    # This assumes you are using a numpy array for your grid
-    width, height = grid.shape
-    glRasterPos2f(-1, -1)
-    glDrawPixels(width, height, GL_LUMINANCE, GL_FLOAT, grid)
-    glFlush()
-    glutSwapBuffers()
+	def draw_grid():
+		# This assumes you are using a numpy array for your grid
+		width, height = grid.shape
+		glRasterPos2f(-1, -1)
+		glDrawPixels(width, height, GL_LUMINANCE, GL_FLOAT, grid)
+		glFlush()
+		glutSwapBuffers()
 
-def update_grid(new_grid):
-    global grid
-    grid = new_grid
+	def update_grid(new_grid):
+		global grid
+		grid = new_grid
 
 
 
-def init_window(width, height):
-    global window
-    global grid
-    grid = zeros((width, height))
-    glutInit(())
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-    glutInitWindowSize(width, height)
-    glutInitWindowPosition(0, 0)
-    window = glutCreateWindow("Grid filter")
-    glutDisplayFunc(draw_grid)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    #glutMainLoop()
+	def init_window(width, height):
+		global window
+		global grid
+		grid = zeros((width, height))
+		glutInit(())
+		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+		glutInitWindowSize(width, height)
+		glutInitWindowPosition(0, 0)
+		window = glutCreateWindow("Grid filter")
+		glutDisplayFunc(draw_grid)
+		glMatrixMode(GL_PROJECTION)
+		glLoadIdentity()
+		glMatrixMode(GL_MODELVIEW)
+		glLoadIdentity()
+		#glutMainLoop()
 
 
 
